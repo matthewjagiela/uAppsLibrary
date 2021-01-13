6c3b037900726e6f2c0e5e2221442c6fb3624605
Matthew Jagiela <matthewjagiela@icloud.com>
2021-01-13 01:09:49 -0500

    Pod Test

---
+++ uAppsLibrary/TestFile.swift
@@ -0,0 +1,17 @@
+//
+//  TestFile.swift
+//  uAppsLibrary
+//
+//  Created by Matthew Jagiela on 1/13/21.
+//
+
+import Foundation
+
+public class TestuApps {
+    public init() {
+        
+    }
+    public func testMe() {
+        print("tested")
+    }
+}
---
+++ uAppsLibrary/uAppsLibrary.h
@@ -0,0 +1,18 @@
+//
+//  uAppsLibrary.h
+//  uAppsLibrary
+//
+//  Created by Matthew Jagiela on 1/13/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for uAppsLibrary.
+FOUNDATION_EXPORT double uAppsLibraryVersionNumber;
+
+//! Project version string for uAppsLibrary.
+FOUNDATION_EXPORT const unsigned char uAppsLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <uAppsLibrary/PublicHeader.h>
+
+
