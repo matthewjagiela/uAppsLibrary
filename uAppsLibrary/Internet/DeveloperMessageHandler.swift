fdeb13453d4fb3371d4750c446e51f736c725364
Matthew Jagiela <matthewjagiela@icloud.com>
2021-01-15 12:17:40 -0500

    feature: Developer Message

---
+++ uAppsLibrary/Internet/DeveloperMessageHandler.swift
@@ -0,0 +1,100 @@
+//
+//  DeveloperMessageHandler.swift
+//  uAppsLibrary
+//
+//  Created by Matthew Jagiela on 1/15/21.
+
+import Combine
+
+public enum App {
+    case uTime
+    case uSurf
+}
+public class DeveloperMessage {
+    fileprivate let jsonURL = URL(string: "https://raw.githubusercontent.com/matthewjagiela/uApps-JSON/messageme/uAppsInfo.json")
+    public init() {}
+    
+    @available(iOS 13.0, *)
+    public func developerMessage() -> Future<Message, InternetJSONError> {
+        return Future { promise in
+            if let jsonURL = self.jsonURL {
+                URLSession.shared.dataTask(with: jsonURL) { data, _, error in
+                    if let error = error {
+                        promise(.failure(.fetchError(error)))
+                    } else {
+                        if let fetchedData = data {
+                            let decoder = JSONDecoder()
+                            do {
+                                let developerMessage = try decoder.decode(Message.self, from: fetchedData)
+                                promise(.success(developerMessage))
+                            } catch {
+                                promise(.failure(.decodeError))
+                            }
+                        }
+                    }
+                }.resume()
+            } else {
+                return promise(.failure(.genericError))
+            }
+        }
+    }
+    
+    public func legacyDeveloperMessage(completion: @escaping(Message) -> Void) {
+        var developerMessage: Message?
+        if let jsonURL = self.jsonURL {
+            URLSession.shared.dataTask(with: jsonURL) { data, _, error in
+                if let fetchedData = data {
+                    let decoder = JSONDecoder()
+                    do {
+                        developerMessage = try decoder.decode(Message.self, from: fetchedData)
+                        if let developerMessage = developerMessage {
+                            completion(developerMessage)
+                        } else {
+                            completion(Message())
+                        }
+                        
+                    } catch {
+                        print("An Error Has Occured \(error)")
+                        completion(Message())
+                    }
+                }
+            }.resume()
+        }
+    }
+}
+
+open class Message: NSObject, Decodable {
+    public var uTimeMessage: MessageDetails?
+    public var uSurfMessage: MessageDetails?
+    enum CodingKeys: String, CodingKey {
+        case message = "messages"
+        case uTimeMessage = "uTime"
+        case uSurfMessage = "uSurf"
+    }
+    override init() {
+        super.init()
+    }
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let nestedContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .message)
+        uTimeMessage = try nestedContainer.decode(MessageDetails.self, forKey: .uTimeMessage)
+        uSurfMessage = try nestedContainer.decode(MessageDetails.self, forKey: .uSurfMessage)
+    }
+}
+
+open class MessageDetails: Decodable {
+    public var announcementNumber: Int
+    public var message: String
+    public var messageActive: Bool
+    enum CodingKeys: String, CodingKey {
+        case announcementNumber = "Announcement_Number"
+        case message = "Message"
+        case messageActive = "Message_Active"
+    }
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        announcementNumber = try container.decode(Int.self, forKey: .announcementNumber)
+        message = try container.decode(String.self, forKey: .message)
+        messageActive = try container.decode(Bool.self, forKey: .messageActive)
+    }
+}
--- uAppsLibraryTests/uAppsInternetTests.swift
+++ uAppsLibraryTests/uAppsInternetTests.swift
@@ -11,7 +11,7 @@ import Combine
 class uAppsInternetTests: XCTestCase {
     
     let handler = InternetLabelsManager()
-    var subsciptions: Set<AnyCancellable> = []
+    var subscriptions: Set<AnyCancellable> = []
     
     func testInternetLabels() {
         let internetExpectation = expectation(description: "InternetLabels")
@@ -28,7 +28,7 @@ class uAppsInternetTests: XCTestCase {
             XCTAssertNotNil(info.uSurfVersion)
             internetExpectation.fulfill()
             
-        }).store(in: &subsciptions)
+        }).store(in: &subscriptions)
         waitForExpectations(timeout: 5)
     }
     
@@ -40,6 +40,36 @@ class uAppsInternetTests: XCTestCase {
             XCTAssertNotNil(info.uFailVersion)
             XCTAssertNotNil(info.uSurfVersion)
             internetExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 5)
+    }
+    
+    func testDeveloperMessage() {
+        let handler = DeveloperMessage()
+        let devMessageExpectation = expectation(description: "DevMessage")
+        handler.developerMessage().receive(on: RunLoop.main).sink(receiveCompletion: { completion in
+            switch completion {
+            case.failure(let error): XCTFail("Failure with error: \(error)")
+            case .finished: print("finished")
+            }
+        }, receiveValue: { message in
+            XCTAssertNotNil(message.uSurfMessage)
+            XCTAssertNotNil(message.uTimeMessage)
+            XCTAssertNotNil(message.uTimeMessage?.announcementNumber)
+            devMessageExpectation.fulfill()
+        }).store(in: &subscriptions)
+        
+        waitForExpectations(timeout: 5)
+    }
+    
+    func testLegacyDeveloperMessage() {
+        let handler = DeveloperMessage()
+        let devMessageexpectation = expectation(description: "DevMessage")
+        handler.legacyDeveloperMessage { message in
+            XCTAssertNotNil(message.uSurfMessage?.announcementNumber)
+            XCTAssertNotNil(message.uSurfMessage?.message)
+            XCTAssertNotNil(message.uSurfMessage?.messageActive)
+            devMessageexpectation.fulfill()
         }
         waitForExpectations(timeout: 5)
     }
